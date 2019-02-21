2e7cfb29965b82c2494258986b5f634425b1ed10
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2019-02-21 14:59:21 -0500

    Changed port number, still working on the rest

--- run.py
+++ run.py
@@ -3,10 +3,11 @@
 import http.server
 import socketserver
 
-PORT = 8000
+PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     print(str(httpd.get_request()))
+    print(str(httpd.server_address))
     httpd.serve_forever()
